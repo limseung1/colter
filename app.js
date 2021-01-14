192e697bcc5b4d715d8519dde988e989fc079cfb
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-14 18:49:09 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+document.getElementById("skill-tab").addEventListener("click", e => {
+    document.getElementById("landing-page").style.display = "none"
+    document.getElementById("skill-page").style.display = "block"
+})
+// document.getElementById("skill-tab").addEventListener("click", e => {
+//     document.getElementById("landing-page").style.display = "none"
+//     document.getElementById("skill-page").style.display = "block"
+//     document.getElementById("education-page").style.display = "none"
+//     document.getElementById("project-page").style.display = "none"
+//     document.getElementById("certification-page").style.display = "none"
+// })
+// document.getElementById("skill-tab").addEventListener("click", e => {
+//     document.getElementById("landing-page").style.display = "none"
+//     document.getElementById("skill-page").style.display = "block"
+//     document.getElementById("education-page").style.display = "none"
+//     document.getElementById("project-page").style.display = "none"
+//     document.getElementById("certification-page").style.display = "none"
+// })
+// document.getElementById("skill-tab").addEventListener("click", e => {
+//     document.getElementById("landing-page").style.display = "none"
+//     document.getElementById("skill-page").style.display = "block"
+//     document.getElementById("education-page").style.display = "none"
+//     document.getElementById("project-page").style.display = "none"
+//     document.getElementById("certification-page").style.display = "none"
+// })
+// document.getElementById("skill-tab").addEventListener("click", e => {
+//     document.getElementById("landing-page").style.display = "none"
+//     document.getElementById("skill-page").style.display = "block"
+//     document.getElementById("education-page").style.display = "none"
+//     document.getElementById("project-page").style.display = "none"
+//     document.getElementById("certification-page").style.display = "none"
+// })
