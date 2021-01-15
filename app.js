6bc49fdce816d53946770f75f50b5dcb075d9e44
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-15 15:09:27 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 document.getElementById("skill-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
     document.getElementById("skill-page").style.display = "block"
+    document.getElementById("education-page").style.display = "none"
 })
-// document.getElementById("skill-tab").addEventListener("click", e => {
-//     document.getElementById("landing-page").style.display = "none"
-//     document.getElementById("skill-page").style.display = "block"
-//     document.getElementById("education-page").style.display = "none"
-//     document.getElementById("project-page").style.display = "none"
-//     document.getElementById("certification-page").style.display = "none"
-// })
+
+document.getElementById("education-tab").addEventListener("click", e => {
+    document.getElementById("landing-page").style.display = "none"
+    document.getElementById("skill-page").style.display = "none"
+    document.getElementById("education-page").style.display = "block"
+})
 // document.getElementById("skill-tab").addEventListener("click", e => {
 //     document.getElementById("landing-page").style.display = "none"
 //     document.getElementById("skill-page").style.display = "block"
