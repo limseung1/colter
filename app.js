c817425fcd090b2abd14c14c8947a25eb98c38da
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-26 13:06:22 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 document.getElementById("skill-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
-    document.getElementById("skill-page").style.display = "block"
     document.getElementById("education-page").style.display = "none"
+    document.getElementById("experience-page").style.display = "none"
+    document.getElementById("skill-page").style.display = "block"
 })
 
 document.getElementById("education-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
-    document.getElementById("skill-page").style.display = "none"
     document.getElementById("education-page").style.display = "block"
+    document.getElementById("experience-page").style.display = "none"
+    document.getElementById("skill-page").style.display = "none"
 })
-// document.getElementById("skill-tab").addEventListener("click", e => {
-//     document.getElementById("landing-page").style.display = "none"
-//     document.getElementById("skill-page").style.display = "block"
-//     document.getElementById("education-page").style.display = "none"
-//     document.getElementById("project-page").style.display = "none"
-//     document.getElementById("certification-page").style.display = "none"
-// })
-// document.getElementById("skill-tab").addEventListener("click", e => {
-//     document.getElementById("landing-page").style.display = "none"
-//     document.getElementById("skill-page").style.display = "block"
-//     document.getElementById("education-page").style.display = "none"
-//     document.getElementById("project-page").style.display = "none"
-//     document.getElementById("certification-page").style.display = "none"
-// })
-// document.getElementById("skill-tab").addEventListener("click", e => {
-//     document.getElementById("landing-page").style.display = "none"
-//     document.getElementById("skill-page").style.display = "block"
-//     document.getElementById("education-page").style.display = "none"
-//     document.getElementById("project-page").style.display = "none"
-//     document.getElementById("certification-page").style.display = "none"
-// })
+document.getElementById("experience-tab").addEventListener("click", e => {
+    document.getElementById("landing-page").style.display = "none"
+    document.getElementById("education-page").style.display = "none"
+    document.getElementById("experience-page").style.display = "block"
+    document.getElementById("skill-page").style.display = "none"
+})
