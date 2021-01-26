1a2099424d92ad767022f95b440127bd90b8fc1c
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-26 18:30:30 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Navigation : hide and show section
 document.getElementById("skill-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
     document.getElementById("education-page").style.display = "none"
@@ -17,3 +19,17 @@ document.getElementById("experience-tab").addEventListener("click", e => {
     document.getElementById("experience-page").style.display = "block"
     document.getElementById("skill-page").style.display = "none"
 })
+
+
+// Copy to Clipboard
+function copy_to_clipboard(value) {
+    document.querySelector(".clipboard-msg").innerHTML = "Copied!"
+    var tempInput = document.createElement("input");
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+  }
+  
