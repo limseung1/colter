d021de1f064a24547eb926e9c115b8067c4c8298
limseung1 <54563657+limseung1@users.noreply.github.com>
2021-01-27 19:25:26 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ document.getElementById("skill-tab").addEventListener("click", e => {
     document.getElementById("skill-page").style.display = "block"
     document.getElementById("project-page").style.display = "none"
     document.getElementById("certification-page").style.display = "none"
+
+    document.querySelectorAll(".selection-control").forEach((control)=>{
+        control.style["border-bottom"] = "none"
+        control.style["padding-bottom"] = "none"
+    })
+    e.target.style["border-bottom"] = "#f4f4f4 solid 4px"
+    e.target.style["padding-bottom"] = "0.25rem"
 })
 
 document.getElementById("education-tab").addEventListener("click", e => {
@@ -16,6 +23,13 @@ document.getElementById("education-tab").addEventListener("click", e => {
     document.getElementById("skill-page").style.display = "none"
     document.getElementById("project-page").style.display = "none"
     document.getElementById("certification-page").style.display = "none"
+
+    document.querySelectorAll(".selection-control").forEach((control)=>{
+        control.style["border-bottom"] = "none"
+        control.style["padding-bottom"] = "none"
+    })
+    e.target.style["border-bottom"] = "#f4f4f4 solid 4px"
+    e.target.style["padding-bottom"] = "0.25rem"
 })
 document.getElementById("experience-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
@@ -24,6 +38,13 @@ document.getElementById("experience-tab").addEventListener("click", e => {
     document.getElementById("skill-page").style.display = "none"
     document.getElementById("project-page").style.display = "none"
     document.getElementById("certification-page").style.display = "none"
+
+    document.querySelectorAll(".selection-control").forEach((control)=>{
+        control.style["border-bottom"] = "none"
+        control.style["padding-bottom"] = "none"
+    })
+    e.target.style["border-bottom"] = "#f4f4f4 solid 4px"
+    e.target.style["padding-bottom"] = "0.25rem"
 })
 document.getElementById("project-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
@@ -32,6 +53,13 @@ document.getElementById("project-tab").addEventListener("click", e => {
     document.getElementById("skill-page").style.display = "none"
     document.getElementById("project-page").style.display = "block"
     document.getElementById("certification-page").style.display = "none"
+
+    document.querySelectorAll(".selection-control").forEach((control)=>{
+        control.style["border-bottom"] = "none"
+        control.style["padding-bottom"] = "none"
+    })
+    e.target.style["border-bottom"] = "#f4f4f4 solid 4px"
+    e.target.style["padding-bottom"] = "0.25rem"
 })
 document.getElementById("certification-tab").addEventListener("click", e => {
     document.getElementById("landing-page").style.display = "none"
@@ -40,6 +68,13 @@ document.getElementById("certification-tab").addEventListener("click", e => {
     document.getElementById("skill-page").style.display = "none"
     document.getElementById("project-page").style.display = "none"
     document.getElementById("certification-page").style.display = "block"
+
+    document.querySelectorAll(".selection-control").forEach((control)=>{
+        control.style["border-bottom"] = "none"
+        control.style["padding-bottom"] = "none"
+    })
+    e.target.style["border-bottom"] = "#f4f4f4 solid 4px"
+    e.target.style["padding-bottom"] = "0.25rem"
 })
 
 
@@ -57,6 +92,9 @@ function copy_to_clipboard(value) {
     setTimeout(function(){
         document.querySelector(".clipboard-msg").style["visibility"] = "hidden"
     }, 1000);
-
   }
   
+// Menu selection update
+function update_menu(item){
+    this.item.style["border-bottom"] =  "#f4f4f4 solid 4px;"
+}
